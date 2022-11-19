b48d53c980935fec9501f6da0e88d673fc3c7857
yushanwebdev <yushanwebdev@gmail.com>
2022-11-19 18:32:22 +0530

    feat: Adding Animation Value - Configure the `scrollOffset` Shared Value in the `<Album />`

--- components/Album.tsx
+++ components/Album.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
 import { View, StyleSheet } from "react-native";
 
-import {
-  Album, MIN_HEADER_HEIGHT, HEADER_DELTA,
-} from "./Model";
+import { Album, MIN_HEADER_HEIGHT } from "./Model";
 import Header from "./Header";
 import Content from "./Content";
 import Cover from "./Cover";
 import ShufflePlay, { BUTTON_HEIGHT } from "./ShufflePlay";
+import {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
 
 interface AlbumProps {
   album: Album;
@@ -15,11 +17,13 @@ interface AlbumProps {
 
 export default ({ album }: AlbumProps) => {
   const { artist } = album;
+  const scrollOffset = useSharedValue(0);
+
   return (
     <View style={styles.container}>
-      <Cover {...{ album }} />
-      <Content {...{ album }} />
-      <Header {...{ artist }} />
+      <Cover {...{ scrollOffset, album }} />
+      <Content {...{ scrollOffset, album }} />
+      <Header {...{ scrollOffset, artist }} />
       <View
         style={{
           position: "absolute",
