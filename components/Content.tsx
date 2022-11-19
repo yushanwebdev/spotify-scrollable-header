12ed97e15829cc3945203d87ee0cf5346cfabffc
yushanwebdev <yushanwebdev@gmail.com>
2022-11-19 18:42:14 +0530

    feat: Adding Animation Value - Develop to update the `scrollOffset` when scrolling the `<Content />`

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,28 +1,37 @@
 import * as React from "react";
-import {
-  StyleSheet, View, Text, ScrollView,
-} from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
-import { Album, MAX_HEADER_HEIGHT, HEADER_DELTA } from "./Model";
+import { Album, MAX_HEADER_HEIGHT } from "./Model";
 import Track from "./Track";
+import Animated, {
+  SharedValue,
+  useAnimatedScrollHandler,
+} from "react-native-reanimated";
 
 interface ContentProps {
   album: Album;
+  scrollOffset: SharedValue<number>;
 }
 
-export default ({ album: { artist, tracks } }: ContentProps) => {
+export default ({ album: { artist, tracks }, scrollOffset }: ContentProps) => {
   const height = MAX_HEADER_HEIGHT;
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      scrollOffset.value = event.contentOffset.y;
+    },
+  });
+
   return (
-    <ScrollView
+    <Animated.ScrollView
+      onScroll={scrollHandler}
       style={styles.container}
       showsVerticalScrollIndicator={false}
       scrollEventThrottle={1}
     >
       <View style={styles.header}>
-        <View
-          style={[styles.gradient, { height }]}
-        >
+        <View style={[styles.gradient, { height }]}>
           <LinearGradient
             style={StyleSheet.absoluteFill}
             start={[0, 0.3]}
@@ -35,16 +44,11 @@ export default ({ album: { artist, tracks } }: ContentProps) => {
         </View>
       </View>
       <View style={styles.tracks}>
-        {
-          tracks.map((track, key) => (
-            <Track
-              index={key + 1}
-              {...{ track, key, artist }}
-            />
-          ))
-        }
+        {tracks.map((track, key) => (
+          <Track index={key + 1} {...{ track, key, artist }} />
+        ))}
       </View>
-    </ScrollView>
+    </Animated.ScrollView>
   );
 };
 
